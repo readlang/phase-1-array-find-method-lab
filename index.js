b4f506b7089c718904f1d9b8814950ad0e9da8b0
Read Langworthy <rlangworthy@gmail.com>
2021-10-18 17:30:41 -0700

    commit tree ad71ede6450b5035f8ebfa01c12d042d3b55e7ea

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 
+const record = [
+    { year: "1971", result: "N/A"},
+    { year: "1970", result: "N/A"},
+    { year: "1969", result: "L"},
+    { year: "1968", result: "N/A"},
+    { year: "1967", result: "N/A"},
+    { year: "1966", result: "L"},
+    { year: "1965", result: "N/A"},
+    { year: "1964", result: "N/A"},
+]
+
+
+
+function superbowlWin(arr) {
+    let newVal =  arr.find(callback)
+    console.log(newVal)
+    if (newVal === undefined) {
+        return(undefined)
+    }
+    return (newVal.year)  
+    
+}
+
+function callback(element, index, array) {
+    return (element.result === "W") 
+}
+
+
+// console.log(
+// superbowlWin(record)
+// )
